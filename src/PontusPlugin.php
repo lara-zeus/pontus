b9db1cec9a71ae3d2cc7dcbdb33fd1f00bc40437
Ash Monsh <wh7r.com@gmail.com>
2025-12-06 09:00:31 +0300

    Update PontusPlugin.php

--- src/PontusPlugin.php
+++ src/PontusPlugin.php
@@ -14,7 +14,6 @@ use LaraZeus\Pontus\Filament\Resources\PlanSubscriptionResource;
 
 final class PontusPlugin implements Plugin
 {
-    // test phpstan
     use CanHideResources;
     use EvaluatesClosures;
     use HasNavigationGroupLabel;
