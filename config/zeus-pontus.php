9fcef4f376b2f659175bee63c25030a2f834d0e8
atmonshi <1952412+atmonshi@users.noreply.github.com>
2026-04-21 12:37:12 +0000

    Fix styling

--- config/zeus-pontus.php
+++ config/zeus-pontus.php
@@ -1,14 +1,20 @@
 <?php
 
+use LaraZeus\Pontus\Enums\Features;
+use LaraZeus\Pontus\Enums\Intervals;
+use LaraZeus\Pontus\Models\Invoices\Invoice;
+use LaraZeus\Pontus\Models\Invoices\InvoiceDetails;
+use LaraZeus\Pontus\Models\Invoices\InvoiceTransactions;
+
 return [
     /**
      * you can overwrite any model and use your own
      */
     'models' => [
-        'Invoice' => \LaraZeus\Pontus\Models\Invoices\Invoice::class,
-        'InvoiceDetails' => \LaraZeus\Pontus\Models\Invoices\InvoiceDetails::class,
-        'InvoiceTransactions' => \LaraZeus\Pontus\Models\Invoices\InvoiceTransactions::class,
-        'Features' => \LaraZeus\Pontus\Enums\Features::class,
-        'Intervals' => \LaraZeus\Pontus\Enums\Intervals::class,
+        'Invoice' => Invoice::class,
+        'InvoiceDetails' => InvoiceDetails::class,
+        'InvoiceTransactions' => InvoiceTransactions::class,
+        'Features' => Features::class,
+        'Intervals' => Intervals::class,
     ],
 ];
